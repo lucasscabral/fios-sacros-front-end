40dde517d38d259ebaee6107af0e6f486bcc4cae
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-04 11:37:19 -0300

    feat: Adiciona novo componente no arquivo app.js

--- src/App/App.js
+++ src/App/App.js
@@ -4,6 +4,7 @@ import useContextAPI from "../contexts/useContext.js";
 import LoginScreen from "../components/authentication/loginScreen";
 import RegistrationScreen from "../components/authentication/registrationScreen";
 import "../assets/style/reset.css"
+import HomeScreen from "../components/home/homeScreen.js";
 
 function App() {
   const [token, setToken] = useState("")
@@ -12,13 +13,14 @@ function App() {
     name: "",
     profile_url: "",
   })
-
   console.log(dataUser)
+
   return (
     <useContextAPI.Provider value={{ token, setToken, dataUser, setDataUser }}>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={<LoginScreen />} />
+          <Route path="/" element={<HomeScreen />} />
+          <Route path="/signin" element={<LoginScreen />} />
           <Route path="/signup" element={<RegistrationScreen />} />
         </Routes>
       </BrowserRouter>
