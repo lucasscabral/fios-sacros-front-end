6e985b0220db6f42d84cc5064b3c8cd658f78ff1
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-07 17:02:54 -0300

    feat: Implementa o componente do carrinho de compras e a rota dela.

--- src/App/App.js
+++ src/App/App.js
@@ -3,11 +3,13 @@ import { useState } from "react";
 import useContextAPI from "../contexts/useContext.js";
 import LoginScreen from "../components/authentication/loginScreen";
 import RegistrationScreen from "../components/authentication/registrationScreen";
-import "../assets/style/reset.css"
 import HomeScreen from "../components/home/homeScreen.js";
+import ShoppingCartScreen from "../components/shoppingCart/shoppingCartScreen.js";
+import "../assets/style/reset.css"
 
 function App() {
   const [token, setToken] = useState("")
+  const [categories, setCategories] = useState()
   const [dataUser, setDataUser] = useState({
     id: "",
     name: "",
@@ -16,10 +18,11 @@ function App() {
   console.log(dataUser)
 
   return (
-    <useContextAPI.Provider value={{ token, setToken, dataUser, setDataUser }}>
+    <useContextAPI.Provider value={{ token, setToken, dataUser, setDataUser, categories, setCategories }}>
       <BrowserRouter>
         <Routes>
           <Route path="/" element={<HomeScreen />} />
+          <Route path="/shopping-cart" element={<ShoppingCartScreen />} />
           <Route path="/signin" element={<LoginScreen />} />
           <Route path="/signup" element={<RegistrationScreen />} />
         </Routes>
