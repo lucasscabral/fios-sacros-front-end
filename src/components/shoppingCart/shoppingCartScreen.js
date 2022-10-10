10216398a16dbd10042e3693640fa8faf3f2a98e
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-10 15:50:19 -0300

    feat: Implementa as variáveis de dependência no array do useEffect

--- src/components/shoppingCart/shoppingCartScreen.js
+++ src/components/shoppingCart/shoppingCartScreen.js
@@ -1,51 +1,54 @@
-// import { Box } from "@mui/material";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useContext } from "react";
 import "../../assets/style/fonts.css"
 import styled from "styled-components";
-import PhotoProduct from "../../assets/images/boneca-de-pano.jpg"
-import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
-import { Button } from "@mui/material";
-import NavBar from "../navBar/navBar.js"
+import NavBarShoppingCartScreen from "./navBarShoppingCartScreen";
+import LocalGroceryStoreOutlinedIcon from '@mui/icons-material/LocalGroceryStoreOutlined';
+import { Link } from "react-router-dom";
+import axios from "axios";
+import API_URL from "../../utils/apiUrl";
+import useContextAPI from "../../contexts/useContext";
+import ListProductsInShoppingCart from "./listProductsInShoppingCart";
 
 export default function ShoppingCartScreen() {
+    const [shoppingCart, setShoppingCart] = useState([])
+    const [deleteProduct, setDeleteProduct] = useState(false)
+    const { token, finalizePurchase } = useContext(useContextAPI)
 
+
+
+    useEffect(() => {
+        const config = {
+            headers: {
+                Authorization: `Bearer ${token}` || ""
+            }
+        }
+        const allProductsShoppingCart = axios.get(`${API_URL}/shopping-cart`, config)
+        allProductsShoppingCart.then(res => {
+            const allProducts = res.data
+            setShoppingCart(allProducts)
+        }).catch(_ => {
+            setShoppingCart([])
+        })
+    }, [finalizePurchase, token, deleteProduct])
 
     return (
         <>
-            <NavBar />
+            <NavBarShoppingCartScreen />
             <Main>
                 <DivTitle>
                     <Title>Carrinho de Compras</Title>
                 </DivTitle>
-                <Container>
-                    <ContainerPurchases>
-                        <BoxPurchase >
-                            <TitlePurchases>MINHAS COMPRAS</TitlePurchases>
-                            <Purchase>
-                                <BoxImage>
-                                    <ImageProduct src={PhotoProduct} />
-                                </BoxImage>
-                                <OrderDetails>
-                                    <Price>R$ 10.00</Price>
-                                    <NameProduct>Boneca de pano</NameProduct>
-                                    <CloseOutlinedIcon style={{ position: "absolute", top: 10, right: 20, cursor: "pointer" }} />
-                                </OrderDetails>
-                            </Purchase>
-                        </BoxPurchase>
-                        <Box >
-                            <TitlePurchases>TOTAL</TitlePurchases>
-                            <DetailsCheckOut>
-                                <DivSubTotal>
-                                    <span>Sub-total</span>
-                                    <span>R$ 10.00</span>
-                                </DivSubTotal>
-                            </DetailsCheckOut>
-                            <DivButton>
-                                <Button variant="contained" color="success">FINALIZAR COMPRA</Button>
-                            </DivButton>
-                        </Box>
-                    </ContainerPurchases>
-                </Container>
+                {
+                    shoppingCart.length === 0 ?
+                        <ContainerEmptyCart>
+                            <LocalGroceryStoreOutlinedIcon sx={{ m: 1, width: 80, height: 80, color: "white" }} />
+                            <DivTitleEmptyCart>
+                                <h2>Seu carrinho está vazio</h2>
+                                <span>Os produtos que você deseja estão lhe esperenado,<Link to={"/"} style={{ textDecoration: "none" }}>clique aqui</Link> </span>
+                            </DivTitleEmptyCart>
+                        </ContainerEmptyCart> :
+                        <ListProductsInShoppingCart shoppingCart={shoppingCart} deleteProduct={deleteProduct} setDeleteProduct={setDeleteProduct} />
+                }
             </Main>
         </>
     )
@@ -55,13 +58,9 @@ const Main = styled.div`
     margin-top: 50px;
     background-color: lightgray;
     min-height: 100vh;
-`
-
-const Container = styled.div`
-    display: flex;
-    margin-top: 20px;
-    justify-content: center;
-    width: 100%;
+    @media (max-width:600px) {
+        margin-top: 80px;
+    }
 `
 
 const DivTitle = styled.div`
@@ -71,6 +70,9 @@ const DivTitle = styled.div`
     background-color: white;
     width: 100%;
     height: 150px;
+    @media (max-width:600px) {
+        text-align: center;
+    }
 `
 
 const Title = styled.h1`
@@ -81,104 +83,31 @@ const Title = styled.h1`
     font-size: 50px;
 `
 
-const ContainerPurchases = styled.div`
-    display: flex;
-    width: 80%;
-`
-
-const BoxPurchase = styled.div`
-    padding: 20px;
-    box-sizing: border-box;
-    background-color: white;
-    width: 60%;
-    height: 100%;
-    margin-right: 20px;
-`
-
-const Box = styled.div`
-    padding: 20px;
-    box-sizing: border-box;
-    background-color: white;
-    width: 40%;
+const ContainerEmptyCart = styled.div`
+    display:flex;
+    flex-direction: column;
+    width: 100%;
     height: 300px;
-`
-
-const TitlePurchases = styled.h2`
-    font-family: 'Lalezar', cursive;
-    font-style: "normal";
-    color: gray;
-    font-size: 30px;
-`
-
-const Purchase = styled.div`
-    display:flex;
-    justify-content: space-between;
-    padding: 10px;
-    box-sizing: border-box;
-    margin-top: 10px;
-    border-top:  1px solid lightgray ;
-    width: 100%;
-`
-
-const BoxImage = styled.div`
-    width: 120px;
-    height: 100%;
-`
-
-const ImageProduct = styled.img`
-    width: 100%;
-    height: 100%;
-`
-
-const OrderDetails = styled.div`
-    width: 78%;
-    position: relative;
-    font-family: 'Lalezar', cursive;
-    font-style: normal;
-`
-
-const Price = styled.h3`
-    position: absolute;
-    top:20px;
-    font-size: 22px;
-    font-style: normal;
-    color: gray;
-`
-
-const NameProduct = styled.span`
-    position: absolute;
-    top:40px;
-    font-size: 17px;
-    flex-wrap: wrap;
-    color: #A9A9A9;
-`
-
-const DetailsCheckOut = styled.div`
-    display:flex;
-    justify-content: space-between;
-    padding: 10px;
-    box-sizing: border-box;
-    margin-top: 10px;
-    border-top:  1px solid lightgray ;
-    width: 100%;
-`
-
-const DivSubTotal = styled.div`
-    display: flex;
-    justify-content: space-between;
-    width: 100%;
-    font-family: 'Lalezar', cursive;
-    font-style: normal;
-    font-size: 20px;
-    span:nth-child(2){
-        color: gray;
-    }
-`
-
-const DivButton = styled.div`
-    margin-top: 100px;
-    width: 100%;
-    display: flex;
     justify-content: center;
     align-items: center;
 `
+
+const DivTitleEmptyCart = styled.div`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    max-width: 450px;
+    height: 100px;
+    text-align: center;
+    font-family: 'Varela Round', sans-serif;
+    h2{
+       font-weight: bold;
+       font-size: 20px;
+       margin-bottom: 20px;
+    }
+    span{
+        font-size: 17px;
+    }
+`
+
