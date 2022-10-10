7af5f2c7530cc9026b496ee280c3c1f87b74f0aa
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-10 15:48:46 -0300

    feat: Implementa as funcionalidades de finalizar a compra e remoção de um produto no carrinho

--- src/components/shoppingCart/listProductsInShoppingCart.js
+++ src/components/shoppingCart/listProductsInShoppingCart.js
@@ -1,10 +1,42 @@
 import styled from "styled-components";
 import CloseOutlinedIcon from '@mui/icons-material/CloseOutlined';
 import { Button } from "@mui/material";
-import { useState } from "react"
+import { useContext, useState } from "react"
+import axios from "axios";
+import useContextAPI from "../../contexts/useContext";
+import API_URL from "../../utils/apiUrl";
+import { Report } from 'notiflix/build/notiflix-report-aio';
+import { Confirm } from 'notiflix/build/notiflix-confirm-aio';
 
 
-function ListProducts({ productId, productImage, productPrice, productName }) {
+function removeProductInShoppingCart(productId, token, deleteProduct, setDeleteProduct) {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}` || ""
+        }
+    }
+
+    Confirm.show(
+        'Confirme a remoção',
+        'Você tem certeza que quer retirar esse produto do seu carrinho de compras?',
+        'Sim',
+        'Não',
+        () => {
+            const deleteUniqueProduct = axios.delete(`${API_URL}/product/${productId}`, config)
+            deleteUniqueProduct.then((_) => {
+                setDeleteProduct(!deleteProduct)
+            }).catch((error) => {
+                console.log(error)
+            })
+        },
+        () => {
+
+        }
+    );
+
+
+}
+function ListProducts({ productId, productImage, productPrice, productName, token, deleteProduct, setDeleteProduct }) {
 
     return (
         <Purchase>
@@ -14,37 +46,60 @@ function ListProducts({ productId, productImage, productPrice, productName }) {
             <OrderDetails>
                 <Price>R$ {productPrice}</Price>
                 <NameProduct>{productName}</NameProduct>
-                <CloseOutlinedIcon style={{ position: "absolute", top: 10, right: 20, cursor: "pointer" }} />
+                <CloseOutlinedIcon style={{ position: "absolute", top: 10, right: 20, cursor: "pointer" }} onClick={() => removeProductInShoppingCart(productId, token, deleteProduct, setDeleteProduct)} />
             </OrderDetails>
         </Purchase>
     )
 
 }
 
+function finalizePurchaseAll(priceTotal, token, finalizePurchase, setFinalizePurchase) {
+    const config = {
+        headers: {
+            Authorization: `Bearer ${token}` || ""
+        }
+    }
+
+    const finalizePurchases = axios.delete(`${API_URL}/delete-shopping-cart`, config)
+    finalizePurchases.then((res) => {
+
+        Report.success(
+            'Muito Obrigado pela preferência',
+            `Sua compra deu R$${priceTotal.toFixed(2)},volte sempre`,
+            'Okay',
+        );
+        setFinalizePurchase(!finalizePurchase)
+    }).catch((error) => {
+
+    })
 
 
-export default function ListProductsInShoppingCart({ shoppingCart }) {
+}
+
+
+
+export default function ListProductsInShoppingCart({ shoppingCart, deleteProduct, setDeleteProduct }) {
+    const { token, finalizePurchase, setFinalizePurchase } = useContext(useContextAPI)
     let priceTotal = 0
     shoppingCart.map(product => priceTotal += Number(product.product.price))
 
-    console.log(priceTotal)
     return (
         <Container>
             <ContainerPurchases>
                 <BoxPurchase >
                     <TitlePurchases>MINHAS COMPRAS</TitlePurchases>
-                    {shoppingCart.map((product, id) => <ListProducts key={id} productId={product.product.id} productImage={product.product.image_url} productPrice={product.product.price} productName={product.product.name} />)}
+                    {shoppingCart.map((product, id) => <ListProducts key={id} productId={product.product.id} productImage={product.product.image_url} productPrice={product.product.price} productName={product.product.name} token={token} deleteProduct={deleteProduct} setDeleteProduct={setDeleteProduct} />)}
                 </BoxPurchase>
                 <Box >
                     <TitlePurchases>TOTAL</TitlePurchases>
                     <DetailsCheckOut>
                         <DivSubTotal>
                             <span>Sub-total</span>
-                            <span>R$ {priceTotal}</span>
+                            <span>R$ {priceTotal.toFixed(2)}</span>
                         </DivSubTotal>
                     </DetailsCheckOut>
                     <DivButton>
-                        <Button variant="contained" color="success">FINALIZAR COMPRA</Button>
+                        <Button variant="contained" color="success" onClick={() => finalizePurchaseAll(priceTotal, token, finalizePurchase, setFinalizePurchase)}>FINALIZAR COMPRA</Button>
                     </DivButton>
                 </Box>
             </ContainerPurchases>
@@ -117,6 +172,10 @@ const Purchase = styled.div`
 const BoxImage = styled.div`
     width: 120px;
     height: 100%;
+    @media (max-width:600px) {
+        width: 90px;
+        margin-right: 15px;
+    }
 `
 
 const ImageProduct = styled.img`
