6971c89a1239fdb8206e60a1b8536b8719b74fda
Lucas Cabral <lucas.9cabral@gmail.com>
2022-10-10 12:35:16 -0300

    Fix:Retorna ao padrão de reenderização do componente App no index

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
-//import React from 'react';
-import ReactDOM from 'react-dom';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
 import App from './app/App.js';
 
 
-ReactDOM.render(<App />, document.querySelector('.root'))
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(
-//   <React.StrictMode>
-//     <App />
-//   </React.StrictMode>
-// );
+// ReactDOM.createRoot(<App />, document.querySelector('.root'))
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
